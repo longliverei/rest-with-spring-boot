011ea7a57a755b03344b98927938c5054904910e
Reinaldo <reinaldoybk@hotmail.com>
2023-09-07 00:28:31 -0300

    First commit

---
+++ FirstSteps/src/main/java/br/com/rei/FirstSteps.java
@@ -0,0 +1,13 @@
+package br.com.rei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSteps {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSteps.class, args);
+	}
+
+}
