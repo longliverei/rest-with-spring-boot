cfbd52080188deb65a3973b0e2ed948148ae569e
Reinaldo <reinaldoybk@hotmail.com>
2023-09-10 04:34:57 -0300

    Rename to Calculator from ParametersAndExceptionHandle

--- Calculator/src/main/java/br/com/rei/Calculator.java
+++ Calculator/src/main/java/br/com/rei/Calculator.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParametersAndExceptionHandler {
+public class Calculator {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ParametersAndExceptionHandler.class, args);
+		SpringApplication.run(Calculator.class, args);
 	}
 
 }
