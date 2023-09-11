71044b6d2fa583ac1ae4e05e8c9dd23f1a069365
Reinaldo <reinaldoybk@hotmail.com>
2023-09-11 17:45:59 -0300

    VO Version2

---
+++ Versioning/src/test/java/br/com/rei/VersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
