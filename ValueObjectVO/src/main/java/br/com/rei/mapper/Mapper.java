0900b38df569d000cc020ab6ffb4cf30f084cb8b
Reinaldo <reinaldoybk@hotmail.com>
2023-09-11 16:08:05 -0300

    Change depreciated Dozer for ModelMapper

--- ValueObjectVO/src/main/java/br/com/rei/mapper/Mapper.java
+++ ValueObjectVO/src/main/java/br/com/rei/mapper/Mapper.java
@@ -3,12 +3,12 @@ package br.com.rei.mapper;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
+import org.modelmapper.ModelMapper;
 
-public class DozerMapper {
+
+public class Mapper {
 	
-	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	private static ModelMapper mapper = new ModelMapper();
 	
 	public static <O, D> D parseObject(O origin, Class<D> destination) {
 		return mapper.map(origin, destination);
