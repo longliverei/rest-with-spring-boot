33bb143be89c38b7b72afeb954c8d9adef89740a
Reinaldo <reinaldoybk@hotmail.com>
2023-09-11 03:18:31 -0300

    Add jpa repository

--- ConnectingToMySQL/src/main/java/br/com/rei/ConnectingToMySqlApplication.java
+++ ConnectingToMySQL/src/main/java/br/com/rei/ConnectingToMySqlApplication.java
@@ -2,8 +2,9 @@ package br.com.rei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ConnectingToMySqlApplication {
 
 	public static void main(String[] args) {
---
+++ ConnectingToMySQL/src/main/java/br/com/rei/services/PersonServices.java
@@ -0,0 +1,76 @@
+package br.com.rei.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.rei.models.Person;
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public List<Person> findAll() {
+		
+		logger.info("finding all people!");
+		
+		List<Person> persons = new ArrayList<>();
+		
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+	
+	public Person findById(String id) {
+		
+		logger.info("finding one person!");
+		
+		Person person = new Person();
+		
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Reinaldo");
+		person.setLastName("Yabiku");
+		person.setAddress("São Paulo - SP -Brasil");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
+	public Person create(Person person) {
+		
+		logger.info("creating a person");
+		return person;
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("updating a person");
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+		logger.info("deleting a person");
+	}
+	
+	public Person mockPerson(int i) {
+		Person person = new Person();
+		
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Some name " + i);
+		person.setLastName("Some last name " + i);
+		person.setAddress("Some address in são mateus " + i);
+		person.setGender("Male");
+		
+		return person;
+	}
+}
