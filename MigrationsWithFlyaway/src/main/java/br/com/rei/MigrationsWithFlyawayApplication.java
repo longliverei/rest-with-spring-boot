751b1bda246a9b95ca31534cd1ab902597788bfa
Reinaldo <reinaldoybk@hotmail.com>
2023-09-11 20:29:04 -0300

    Starting with Migrations

---
+++ MigrationsWithFlyaway/src/main/java/br/com/rei/MigrationsWithFlyawayApplication.java
@@ -0,0 +1,13 @@
+package br.com.rei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationsWithFlyawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationsWithFlyawayApplication.class, args);
+	}
+
+}
