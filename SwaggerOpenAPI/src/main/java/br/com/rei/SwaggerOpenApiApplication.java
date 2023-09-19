f60c532c948b823e0b053d0aef1f03ddfd9cf207
Reinaldo <reinaldoybk@hotmail.com>
2023-09-19 20:51:59 -0300

    Add Swagger dependency in pom

---
+++ SwaggerOpenAPI/src/main/java/br/com/rei/SwaggerOpenApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerOpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerOpenApiApplication.class, args);
+	}
+
+}
