bd532995d7413db7da4c8fd34d3891a773bb7bd5
Reinaldo <reinaldoybk@hotmail.com>
2023-09-20 03:46:15 -0300

    Add @Operation annotation and @ApiReponse for controller in findAll()

--- SwaggerOpenAPI/src/main/java/br/com/rei/controllers/PersonController.java
+++ SwaggerOpenAPI/src/main/java/br/com/rei/controllers/PersonController.java
@@ -16,9 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.rei.data.dto.v1.PersonDto;
 import br.com.rei.services.PersonServices;
 import br.com.rei.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
 public class PersonController {
 	
 	@Autowired
@@ -26,6 +33,21 @@ public class PersonController {
 	
 	@GetMapping(
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	@Operation(summary = "Finds all people", description = "Finds all people",
+			tags = {"People"},
+			responses = {
+				@ApiResponse(description = "Success", responseCode = "200",
+					content = {
+							@Content(
+								mediaType = "application/json",
+								array = @ArraySchema(schema = @Schema(implementation = PersonDto.class))
+							)
+						}),
+				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			})
 	public List<PersonDto> findAll() {
 		return service.findAll();
 	}
