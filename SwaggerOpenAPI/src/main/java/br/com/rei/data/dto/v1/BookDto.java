28ac5bd3461ff1b149efe9d4bee352e158ac2cfa
Reinaldo <reinaldoybk@hotmail.com>
2023-09-20 07:50:32 -0300

    Create bookDto

--- SwaggerOpenAPI/src/main/java/br/com/rei/data/dto/v1/BookDto.java
+++ SwaggerOpenAPI/src/main/java/br/com/rei/data/dto/v1/BookDto.java
@@ -1,95 +1,73 @@
-package br.com.rei.models;
+package br.com.rei.data.dto.v1;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.hateoas.RepresentationModel;
 
-@Entity
-@Table(name = "books")
-public class Book implements Serializable {
+public class BookDto extends RepresentationModel<BookDto> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long key;
 	
-	@Column(name = "author")
+
 	private String author;
-	
-	@Column(name = "launch_date")
 	private String launchDate;
-	
-	@Column(name = "price")
 	private String price;
-	
-	@Column(name = "title")
 	private String title;
-
-	public Long getId() {
-		return id;
+	
+	public Long getKey() {
+		return key;
 	}
-
-	public void setId(Long id) {
-		this.id = id;
+	public void setKey(Long key) {
+		this.key = key;
 	}
-
 	public String getAuthor() {
 		return author;
 	}
-
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-
 	public String getLaunchDate() {
 		return launchDate;
 	}
-
 	public void setLaunchDate(String launchDate) {
 		this.launchDate = launchDate;
 	}
-
 	public String getPrice() {
 		return price;
 	}
-
 	public void setPrice(String price) {
 		this.price = price;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(author, id, launchDate, price, title);
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(author, key, launchDate, price, title);
+		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Book other = (Book) obj;
-		return Objects.equals(author, other.author) && Objects.equals(id, other.id)
+		BookDto other = (BookDto) obj;
+		return Objects.equals(author, other.author) && Objects.equals(key, other.key)
 				&& Objects.equals(launchDate, other.launchDate) && Objects.equals(price, other.price)
 				&& Objects.equals(title, other.title);
 	}
-	
-	
+
 }
