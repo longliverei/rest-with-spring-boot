10acbf731fb8edc31dabee877f44f24729872108
Reinaldo <reinaldoybk@hotmail.com>
2023-09-20 03:56:10 -0300

    Add @Operation and @ApiReponse for controller in other methods

--- SwaggerOpenAPI/src/main/java/br/com/rei/controllers/PersonController.java
+++ SwaggerOpenAPI/src/main/java/br/com/rei/controllers/PersonController.java
@@ -46,7 +46,7 @@ public class PersonController {
 				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
 				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
 				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
 			})
 	public List<PersonDto> findAll() {
 		return service.findAll();
@@ -54,6 +54,19 @@ public class PersonController {
 	
 	@GetMapping(value = "/{id}",
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	@Operation(summary = "Finds a person",
+	description = "Finds a person",
+	tags = {"People"},
+	responses = {
+		@ApiResponse(description = "Success", responseCode = "200",
+			content = @Content(schema = @Schema(implementation = PersonDto.class))
+		),
+		@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public PersonDto findById(@PathVariable("id") Long id) {
 		return service.findById(id);
 	}
@@ -61,6 +74,17 @@ public class PersonController {
 	@PostMapping(
 			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	@Operation(summary = "Add a person",
+	description = "Add a person",
+	tags = {"People"},
+	responses = {
+		@ApiResponse(description = "Created", responseCode = "200",
+			content = @Content(schema = @Schema(implementation = PersonDto.class))
+		),
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public PersonDto create(@RequestBody PersonDto person) {
 		return service.create(person);
 	}
@@ -68,11 +92,32 @@ public class PersonController {
 	@PutMapping(			
 			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	@Operation(summary = "Updates a person",
+	description = "Updates a person",
+	tags = {"People"},
+	responses = {
+		@ApiResponse(description = "Updated", responseCode = "200",
+			content = @Content(schema = @Schema(implementation = PersonDto.class))
+		),
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public PersonDto update(@RequestBody PersonDto person) {
 		return service.update(person);
 	}
 	
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Deletes a person",
+	description = "Deletes a person",
+	tags = {"People"},
+	responses = {
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
