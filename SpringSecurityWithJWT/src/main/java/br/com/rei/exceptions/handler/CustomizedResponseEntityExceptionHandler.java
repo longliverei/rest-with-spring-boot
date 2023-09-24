6171afc619145e975d0cda8ac6c06d63b99f1510
Reinaldo <reinaldoybk@hotmail.com>
2023-09-23 23:09:50 -0300

    Create custom Exception for JWT login

--- SpringSecurityWithJWT/src/main/java/br/com/rei/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ SpringSecurityWithJWT/src/main/java/br/com/rei/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.rei.exceptions.ExceptionResponse;
+import br.com.rei.exceptions.InvalidJwtAuthenticationException;
 import br.com.rei.exceptions.RequiredObjectIsNullException;
 import br.com.rei.exceptions.ResourceNotFoundException;
 
@@ -55,5 +56,18 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(
+			Exception ex,
+			WebRequest request
+			) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 }
