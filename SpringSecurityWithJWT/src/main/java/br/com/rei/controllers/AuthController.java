4b19c155a474da10ae2903ed1ca9165b75cf2911
Reinaldo <reinaldoybk@hotmail.com>
2023-09-25 20:05:00 -0300

    add refreshToken logic

--- SpringSecurityWithJWT/src/main/java/br/com/rei/controllers/AuthController.java
+++ SpringSecurityWithJWT/src/main/java/br/com/rei/controllers/AuthController.java
@@ -3,14 +3,17 @@ package br.com.rei.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rei.data.dto.v1.security.AccountCredentialsDTO;
 import br.com.rei.services.AuthServices;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Authentication Endpoint")
@@ -33,8 +36,25 @@ public class AuthController {
 		
 		return token;
 	}
+	
+	@SuppressWarnings("rawtypes")
+	@Operation(summary = "Refresh token for authenticated user")
+	@PutMapping(value = "/refresh/{username}")
+	public ResponseEntity refreshToken(@PathVariable("username") String username, @RequestHeader("Authorization") String refreshToken) {
+		if (checkIfParamsIsNotNull(username, refreshToken)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request");
+		
+		var token = authServices.refreshToken(username, refreshToken);
+		
+		if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request");
+		
+		return token;
+	}
 
 	private boolean checkIfParamsIsNotNull(AccountCredentialsDTO data) {
 		return data == null || data.getUsername() == null || data.getUsername().isBlank() || data.getPassword() == null || data.getPassword().isBlank();
+	}
+	
+	private boolean checkIfParamsIsNotNull(String username, String refreshToken) {
+		return refreshToken == null || refreshToken.isBlank() || username == null || username.isBlank();
 	}
 }
--- SpringSecurityWithJWT/src/main/java/br/com/rei/services/AuthServices.java
+++ SpringSecurityWithJWT/src/main/java/br/com/rei/services/AuthServices.java
@@ -48,4 +48,20 @@ public class AuthServices {
 			throw new BadCredentialsException("Invalid username/password");
 		}
 	}
+	
+	@SuppressWarnings("rawtypes")
+	public ResponseEntity refreshToken(String username, String refreshToken) {			
+		var user = repository.findByUsername(username);
+			
+		var tokenResponse = new TokenDTO();
+			
+		if (user != null) {
+			tokenResponse = tokenProvider.refreshToken(refreshToken);
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found!");
+		}
+			
+		return ResponseEntity.ok(tokenResponse);
+	}
 }
+
